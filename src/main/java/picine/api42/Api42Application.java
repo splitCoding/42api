b800ad019130981afd8bc9d68886dd0fc831ff64
splitube <voicesplit@daum.net>
2023-01-18 16:18:45 +0900

    feat(Springboot): 스프링 부트 세팅

---
+++ src/main/java/picine/api42/Api42Application.java
@@ -0,0 +1,13 @@
+package picine.api42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api42Application.class, args);
+	}
+
+}
---
+++ src/test/java/picine/api42/Api42ApplicationTests.java
@@ -0,0 +1,13 @@
+package picine.api42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
