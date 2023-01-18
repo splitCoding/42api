fdda1df0cfa942a84a2fb30a4f1a868ed8da642e
splitube <voicesplit@daum.net>
2023-01-18 16:42:19 +0900

    feat(MemberServiceImpl): 서비스 구현

---
+++ src/main/java/picine/api42/member/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package picine.api42.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
